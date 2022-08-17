beb976f6a32f7ce05c6ebd504ade3e3b9096cbcd
İzzet Emre Arı <emreizzet@hotmail.com>
2022-08-17 21:42:24 +0300

    Cok değişilik var

--- pages/health-analysis.jsx
+++ pages/health-analysis.jsx
@@ -1,46 +1,48 @@
+import CustomContainer from 'components/CustomContainer';
+import Footer from 'components/FooterChakra';
+import Header from 'components/Header';
+import WaveCta from 'components/WaveCta';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import styled from 'styled-components';
 import { media } from 'utils/media';
 import HealthAnalysisSection from 'views/HealthAnalysisPage/HealthAnalysisSection';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Footer from 'components/FooterChakra';
-import WaveCta from 'components/WaveCta';
-import Header from 'components/Header';
-import CustomContainer from 'components/CustomContainer';
 export default function HealthAnalysisPage() {
-    return (<>
+  return (
+    <>
       <HealthAnalysisContainer>
-        <Header title="HEALTH ANALYSIS" imgURL="page/hospitals.jpg"/>
+        <Header title="HEALTH ANALYSIS" imgURL="page/hospitals.jpg" />
         <InsideContainer>
           <HealthAnalysisSection />
         </InsideContainer>
       </HealthAnalysisContainer>
       <WaveCta />
       <Footer />
-    </>);
+    </>
+  );
 }
 export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ['common', 'home', 'navbar', 'countries'])),
-            // Will be passed to the page component as props
-        },
-    };
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'home', 'navbar', 'countries'])),
+      // Will be passed to the page component as props
+    },
+  };
 }
-const ContactContainer = styled.div `
+const ContactContainer = styled.div`
   display: flex;
 
   ${media('<=tablet')} {
     flex-direction: column;
   }
 `;
-const HealthAnalysisContainer = styled.div `
+const HealthAnalysisContainer = styled.div`
   z-index: -1;
   background-color: #edeff3;
   background-repeat: no-repeat;
   background-position: right;
   background-size: contain;
 `;
-const InsideContainer = styled(CustomContainer) `
+const InsideContainer = styled(CustomContainer)`
   max-width: 190em;
   margin-top: -150px;
   background-color: rgba(255, 255, 255, 1);
--- views/HealthAnalysisPage/HealthAnalysisSection.jsx
+++ views/HealthAnalysisPage/HealthAnalysisSection.jsx
@@ -1,31 +1,32 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { Button, Checkbox, Input, Select, Stack, Text, VStack } from '@chakra-ui/react';
 import HumanBody from 'components/HumanBody';
-import { Select, Stack, Input, VStack, Text, Checkbox, Button } from '@chakra-ui/react';
-import { create } from 'utils/api';
-import { media } from 'utils/media';
+import { useState } from 'react';
 import Dropdown from 'react-dropdown';
+import styled from 'styled-components';
+import { save } from 'utils/api';
+import { media } from 'utils/media';
 import 'react-dropdown/style.css';
 const options = ['one', 'two', 'three'];
 const defaultOption = options[0];
 export default function HealthAnalysisSection() {
-    const [age, setAge] = useState('');
-    const [height, setHeight] = useState('');
-    const [weight, setWeight] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const saveHealthForm = () => {
-        if (submitted) {
-            return;
-        }
-        create('meta-healths', {
-            name: 'test patient',
-            age,
-            height,
-            weight,
-        });
-        setSubmitted(true);
-    };
-    return (<div className="container">
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const saveHealthForm = () => {
+    if (submitted) {
+      return;
+    }
+    save('meta-healths', {
+      name: 'test patient',
+      age,
+      height,
+      weight,
+    });
+    setSubmitted(true);
+  };
+  return (
+    <div className="container">
       <div className="row">
         <div className="col-md-4">
           <HumanBody />
@@ -34,24 +35,31 @@ export default function HealthAnalysisSection() {
           <div className="row p-5">
             <div className="col-md-6">
               <VStack spacing={4} align="stretch">
-                <Input placeholder="Adınız" h={'5rem'} border={'2px'}/>
-                <Input placeholder="Telefon" h={'5rem'} border={'2px'}/>
-                <Input placeholder="Yaş" h={'5rem'} border={'2px'}/>
-                <Input placeholder="Boy" h={'5rem'} border={'2px'}/>
+                <Input placeholder="Adınız" h={'5rem'} border={'2px'} />
+                <Input placeholder="Telefon" h={'5rem'} border={'2px'} />
+                <Input placeholder="Yaş" h={'5rem'} border={'2px'} />
+                <Input placeholder="Boy" h={'5rem'} border={'2px'} />
               </VStack>
             </div>
 
             <div className="col-md-6">
               <Stack spacing={4} align="stretch">
-                <Input placeholder="Email" h={'5rem'} border={'2px'}/>
-                <Input placeholder="Adres" h={'5rem'} border={'2px'}/>
-                <Input placeholder="Kilo  " h={'5rem'} border={'2px'}/>
-                <Select placeholder="Cinsiyet" h={'5rem'} bg={'#0E92D7'} border={'2px'} color={'white'} _hover={{
-            bg: 'white',
-            borderColor: '#0E92D7',
-            color: '#0E92D7',
-            fontWeight: '500',
-        }}>
+                <Input placeholder="Email" h={'5rem'} border={'2px'} />
+                <Input placeholder="Adres" h={'5rem'} border={'2px'} />
+                <Input placeholder="Kilo  " h={'5rem'} border={'2px'} />
+                <Select
+                  placeholder="Cinsiyet"
+                  h={'5rem'}
+                  bg={'#0E92D7'}
+                  border={'2px'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'white',
+                    borderColor: '#0E92D7',
+                    color: '#0E92D7',
+                    fontWeight: '500',
+                  }}
+                >
                   <option value="option1">Option 1</option>
                   <option value="option2">Option 2</option>
                   <option value="option3">Option 3</option>
@@ -63,12 +71,19 @@ export default function HealthAnalysisSection() {
           <div className="row p-5">
             <div className="col-md-6">
               <Stack spacing={4} align="stretch">
-                <Select placeholder="Ülke" h={'5rem'} bg={'#0E92D7'} border={'2px'} color={'white'} _hover={{
-            bg: 'white',
-            borderColor: '#0E92D7',
-            color: '#0E92D7',
-            fontWeight: '500',
-        }}>
+                <Select
+                  placeholder="Ülke"
+                  h={'5rem'}
+                  bg={'#0E92D7'}
+                  border={'2px'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'white',
+                    borderColor: '#0E92D7',
+                    color: '#0E92D7',
+                    fontWeight: '500',
+                  }}
+                >
                   <option value="option1">Türkiye</option>
                   <option value="option2">ABD</option>
                   <option value="option3">Yunanistan</option>
@@ -77,12 +92,19 @@ export default function HealthAnalysisSection() {
             </div>
             <div className="col-md-6">
               <Stack spacing={4} align="stretch">
-                <Select placeholder="Bölge" h={'5rem'} bg={'#0E92D7'} border={'2px'} color={'white'} _hover={{
-            bg: 'white',
-            borderColor: '#0E92D7',
-            color: '#0E92D7',
-            fontWeight: '500',
-        }}>
+                <Select
+                  placeholder="Bölge"
+                  h={'5rem'}
+                  bg={'#0E92D7'}
+                  border={'2px'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'white',
+                    borderColor: '#0E92D7',
+                    color: '#0E92D7',
+                    fontWeight: '500',
+                  }}
+                >
                   <option value="option1">Option 1</option>
                   <option value="option2">Option 2</option>
                   <option value="option3">Option 3</option>
@@ -92,23 +114,24 @@ export default function HealthAnalysisSection() {
           </div>
           <div className="row p-5">
             <Stack spacing={4} align="stretch">
-              <Textarea placeholder="Açıklama" size="md"/>
+              <Textarea placeholder="Açıklama" size="md" />
               <Checkbox size="lg" m="2rem">
                 <Text fontSize="md" ml="2rem" mt="2rem">
                   I agree to Wetraet Terms and Conditions, I have read the Privacy Policy and I agree that my given details including health
                   data may be processed by Wetreat for the purpose of obtaining quotes.
                 </Text>
               </Checkbox>
-              <Button colorScheme="blue" w={'%50'} h={'5rem'} w={'20%'}>
+              <Button colorScheme="blue" w={'%50'} h={'5rem'}>
                 Gönder
               </Button>
             </Stack>
           </div>
         </div>
       </div>
-    </div>);
+    </div>
+  );
 }
-const InputStack = styled.div `
+const InputStack = styled.div`
   display: flex;
   flex-direction: column;
 
@@ -116,17 +139,17 @@ const InputStack = styled.div `
     margin-top: 0.5rem;
   }
 `;
-const Textarea = styled(Input) `
+const Textarea = styled(Input)`
   width: 100%;
   min-height: 20rem;
 `;
-const CustomDropdown = styled(Dropdown) `
+const CustomDropdown = styled(Dropdown)`
   width: 50%;
   font-size: 1.25rem;
   margin-bottom: 1rem;
   color: black;
 `;
-const CustomLabel = styled.label `
+const CustomLabel = styled.label`
   font-size: 1.5rem;
   col-sm-12or: rgb(var(--text));
   margin-right: 1rem;
