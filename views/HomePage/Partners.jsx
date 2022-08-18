11008091f0a6d0990b64266e8716f98e2f46bae5
Yunus YILMAZ <yilmazyuus01@yandex.com>
2022-08-18 18:43:09 +0300

    fixed warnings

--- views/HomePage/Partners.jsx
+++ views/HomePage/Partners.jsx
@@ -1,11 +1,9 @@
 import CustomContainer from 'components/CustomContainer';
 import NextImage from 'next/image';
-import NextLink from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { media } from 'utils/media';
 const PARTNER_LOGOS = [
   {
     logo:'exen',
@@ -50,19 +48,7 @@ export default function Partners() {
 function normalizePartnerLogoName(logo) {
     return logo.replace('.svg', '');
 }
-const Title = styled.h3 `
-  font-size: 1.3rem;
-  letter-spacing: 0.02em;
-  line-height: 0;
-  text-transform: uppercase;
-  margin-bottom: 2rem;
-  text-align: center;
-  opacity: 0.8;
 
-  ${media('<=desktop')} {
-    line-height: 1.5;
-  }
-`;
 const PartnersWrapper = styled(CustomContainer) `
   .swiper-wrapper {
     will-change: transform;
