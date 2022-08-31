cb258814407a68b8f083910bb43e1d55cf6ff01b
İzzet Emre Arı <emreizzet@hotmail.com>
2022-08-31 19:26:45 +0300

    images

--- views/HomePage/Partners.jsx
+++ views/HomePage/Partners.jsx
@@ -6,50 +6,68 @@ import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 const PARTNER_LOGOS = [
   {
-    logo:'exen',
-    link:'www.exenmakine.com'
+    logo: 'exen',
+    link: 'www.exenmakine.com',
   },
   {
-    logo:'kayseriseker',
-    link:'www.kayseriseker.com'
+    logo: 'kayseriseker',
+    link: 'www.kayseriseker.com',
   },
   {
-    logo:'dirent',
-    link:'www.direntmimarlik.com'
+    logo: 'dirent',
+    link: 'www.direntmimarlik.com',
   },
   {
-    logo:'lojik',
-    link:'www.lojikotomasyon.com'
+    logo: 'lojik',
+    link: 'www.lojikotomasyon.com',
   },
   {
-    logo:'teknokent',
-    link:'www.exenmakine.com'
+    logo: 'teknokent',
+    link: 'www.exenmakine.com',
   },
   {
-    logo:'qatasoft',
-    link:'www.qatasoft.com'
-  }
-  ];
+    logo: 'qatasoft',
+    link: 'www.qatasoft.com',
+  },
+];
 export default function Partners() {
-    return (<PartnersWrapper>
-      <Swiper modules={[Autoplay]} slidesPerView={6} spaceBetween={30} loop={true} autoplay={{ delay: 0, disableOnInteraction: false, pauseOnMouseEnter: false, waitForTransition: false, stopOnLastSlide: false }} speed={3000} breakpoints={{
-            320: { slidesPerView: 2 },
-            768: { slidesPerView: 4 },
-            1025: { slidesPerView: 6 },
-        }} className="swiper-wrapper">
-        {PARTNER_LOGOS.map((company , index) => (<SwiperSlide key={index}>
+  return (
+    <PartnersWrapper>
+      <Swiper
+        modules={[Autoplay]}
+        slidesPerView={6}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{ delay: 0, disableOnInteraction: false, pauseOnMouseEnter: false, waitForTransition: false, stopOnLastSlide: false }}
+        speed={3000}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          768: { slidesPerView: 4 },
+          1025: { slidesPerView: 6 },
+        }}
+        className="swiper-wrapper"
+      >
+        {PARTNER_LOGOS.map((company, index) => (
+          <SwiperSlide key={index}>
             {/* <NextLink href={company.link} locale={false}> */}
-              <NextImage src={'/partners/' + company.logo + '.png'} alt={normalizePartnerLogoName(company.logo)} width={128} height={128}/>
+            <NextImage
+              src={'/images/partners/' + company.logo + '.png'}
+              alt={normalizePartnerLogoName(company.logo)}
+              width={128}
+              height={128}
+            />
             {/* </NextLink> */}
-          </SwiperSlide>))}
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </PartnersWrapper>);
+    </PartnersWrapper>
+  );
 }
 function normalizePartnerLogoName(logo) {
-    return logo.replace('.svg', '');
+  return logo.replace('.svg', '');
 }
 
-const PartnersWrapper = styled(CustomContainer) `
+const PartnersWrapper = styled(CustomContainer)`
   .swiper-wrapper {
     will-change: transform;
     transition-timing-function: linear;
